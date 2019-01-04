8c26e638ff18a3bd29797a63e15d0df74c1b8093
gsrsudha <gsr.sudha8@gmail.com>
2019-01-04 12:39:20 -0600

    animated route transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from './app-animation';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
